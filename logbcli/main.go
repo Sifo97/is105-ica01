1b534c3cf49b135c9d77a3113dabdf6f0bd4e051
uia-student <TestMail@example.com>
2017-01-31 17:30:36 +0100

    La til: logbcli programmet.

---
+++ logbcli/main.go
@@ -0,0 +1,28 @@
+package main
+import "fmt"
+import "os"
+import "math"
+import "strconv"
+func main() {
+// Ta imot argument 1
+logMethod := os.Args[1]
+ArgumentTwo := os.Args[2]
+// Printe det
+fmt.Println("logMethod: " + logMethod)
+x, err := strconv.ParseFloat(ArgumentTwo, 64)
+
+fmt.Print("Feil kode: ")
+fmt.Println(err)
+
+if logMethod == "log10" {
+b := math.Log10(x)
+fmt.Print("Value: ")
+fmt.Println(b)
+}
+if logMethod == "log2" {
+b := math.Log2(x)
+fmt.Print("Value: ")
+fmt.Println(b)
+}
+}
+
