aa4fa9a82133070b741384f235209b76efa5746d
[andere16] <[andere16@uia.no]>
2017-01-31 14:22:09 +0100

    hi

--- hello.go
+++ hello.go
@@ -7,6 +7,10 @@ func swap(x, y string) (string, string) {
 	return y, x
 }
 
+func add(x int, y int) int {
+	return x + y
+}
+
 func main() {
 	fmt.Println("Code below this line:")
 	fmt.Println("---------------------")
@@ -17,4 +21,8 @@ func main() {
 	a = ":) øæå TEST"
 	fmt.Println(a)
 	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Println("532 + 18 = ", add(532, 18))
 }
+
+
+
