627898541e1ed1930be77325b2c736c5c3be78b6
uia-student <TestMail@example.com>
2017-01-31 15:29:08 +0100

    Update

---
+++ log/log.go
@@ -0,0 +1,13 @@
+package log
+import "math"
+import "fmt"
+
+
+func CalcLog(x float64) float64 {
+	math.Log2(x)
+return x
+}
+
+func Ptest() {
+	fmt.Println("TEST")
+}
---
+++ log/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import "log"
+func main() {
+	//fmt.Println(log.calcLog(2))
+	fmt.Println("TEST TEST")
+	Ptest()
+}
+
+
+
